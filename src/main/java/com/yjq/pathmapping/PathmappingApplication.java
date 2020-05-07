533b2e9745b958c6715c8402057433e438003a95
yaojiuqi <393072365@qq.com>
2020-05-08 01:10:02 +0800

    路径映射

---
+++ src/main/java/com/yjq/pathmapping/PathmappingApplication.java
@@ -0,0 +1,13 @@
+package com.yjq.pathmapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PathmappingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PathmappingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yjq/pathmapping/PathmappingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yjq.pathmapping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PathmappingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
